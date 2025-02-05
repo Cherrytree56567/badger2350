3cf26238a479cf19d228a9f8dfb32d87dc6af3b3
Cherrytree56567 <ronitdsilva82@gmail.com>
2025-02-05 20:21:54 +1100

    Update info.py

--- badger_os/examples/info.py
+++ badger_os/examples/info.py
@@ -27,15 +27,15 @@ y = 16 + int(LINE_HEIGHT / 2)
 
 display.text("Made by Pimoroni, powered by MicroPython", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
-display.text("Dual-core RP2040, 133MHz, 264KB RAM", 5, y, WIDTH, TEXT_SIZE)
+display.text("Dual-core RP2350, 150MHz, 8KB RAM", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
-display.text("2MB Flash (1MB OS, 1MB Storage)", 5, y, WIDTH, TEXT_SIZE)
+display.text("16MB Flash (1MB OS, 15MB Storage)", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
 display.text("296x128 pixel Black/White e-Ink", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
 display.text("For more info:", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
-display.text("https://pimoroni.com/badger2040", 5, y, WIDTH, TEXT_SIZE)
+display.text("https://pimoroni.com/badger2350", 5, y, WIDTH, TEXT_SIZE)
 y += LINE_HEIGHT
 display.text(f"Badger OS {version}", 5, y, WIDTH, TEXT_SIZE)
 
